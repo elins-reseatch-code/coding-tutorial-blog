7089ed10343fa0d2f3c3c2e88cdf27f27c449586
ardianto nugroho <ardianto.nugroho@dispendix.com>
2023-12-30 18:32:14 +0700

    fix: forget to add base url for repo name

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,8 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://elins-reseatch-code.github.io/coding-tutorial-blog/",
+	site: "https://elins-reseatch-code.github.io",
+	base: "/coding-tutorial-blog",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		shikiConfig: {
